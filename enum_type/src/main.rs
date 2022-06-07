b8ed5c15871141a5600a9175848663c50136aafb
jujumilk3 <jujumilk3@gmail.com>
2022-06-08 07:53:08 +0900

    enum and null

---
+++ enum_type/src/main.rs
@@ -0,0 +1,50 @@
+// https://rinthel.github.io/rust-lang-book-ko/ch06-01-defining-an-enum.html
+// 열거형과 null에 대한 설명. 유용
+
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main() {
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let home = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback = IpAddr2::V6(String::from("::1"));
+    // 열거형의 각 variant 에 직접 데이터를 붙임으로써, 구조체를 사용할 필요가 없어졌습니다.
+
+
+    let home = IpAddr3::V4(127, 0, 0, 1);
+    let loopback = IpAddr3::V6(String::from("::1"));
+}
