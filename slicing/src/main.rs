ad3e75b38ca860bb1856f9f8485e8f8a9a335bd4
jujumilk3 <jujumilk3@gmail.com>
2022-06-08 07:05:26 +0900

    slicing

---
+++ slicing/src/main.rs
@@ -0,0 +1,59 @@
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word는 5를 갖게 될 것입니다.
+
+    s.clear(); // 이 코드는 String을 비워서 ""로 만들게 됩니다.
+
+    // word는 여기서 여전히 5를 갖고 있지만, 5라는 값을 의미있게 쓸 수 있는 스트링은 이제 없습니다.
+    // word는 이제 완전 유효하지 않습니다!
+
+    let s = String::from("hello world");
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+
+    let s = String::from("hello");
+
+    let slice = &s[0..2];
+    let slice = &s[..2];
+
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[3..len];
+    let slice = &s[3..];
+
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let slice = &s[0..len];
+    let slice = &s[..];
+}
+
+
+fn first_word_index(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
